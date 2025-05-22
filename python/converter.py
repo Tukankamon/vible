85c4787adc4982109cbe445fcbb0f0346dbb405d
TuKankamon <antovedaros@gmail.com>
2025-05-22 21:56:27 +0200

    idk how to do this

--- python/converter.py
+++ python/converter.py
@@ -1,35 +1,23 @@
-#This program converts a txt version on the bible into JSON so it is more readable
-import json 
+#This program converts a txt version on the bible into a more readable format
 
-my_dict = {}
+from dict import bible     #All dictionaries declared here
 
-bible = {
-    "Middleware": ("name", "release"),
-    "System": ("name", "tag"),
-    "Application": ("domain", "host", "user"),
-    "Utility": ("domain", "health", "version"),
-}
 
 with open("test.txt") as file:
     # read through the input file, removing trailing newlines
     lines = [l.strip() for l in file.readlines()]
 
-    # iterate through all the lines in the file
+    counter = 0
+
     for line in lines:
-        # get the details from each line
-        env, key, *field_values = line.split(",")
-        # get the predefined keys from bible
-        field_keys = bible[key]
+        if not line:    #Avoids line breaks
+            pass
 
-        # create the 4 keys if the ENV does not exist
-        # with defaults as empty list
-        if env not in my_dict:
-            my_dict[env] = {k: [] for k in bible}
-
-        # append each new item to the existing list
-        # zip is used to iterate through the keys and values to match them up
-        my_dict[env][key].append({f_k: f_v for f_k, f_v in zip(field_keys, field_values)})
+        elif not str.isdigit(line[0]):      #Looks for a title
+            book = line
+        
+        else:
+            pass #IDK how to do this
 
 
-json_out = {"ENV": my_dict}
-json.dump(json_out, "kjv.json")
+print(bible)
---
+++ test.py
@@ -0,0 +1,2 @@
+list = range(10)
+print(list)
